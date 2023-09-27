e9f18477e148411427d2b7bd9f23a1a5b748e2d1
JVCA101 <joaovictordealmeid@gmail.com>
2023-09-27 20:35:30 -0300

    Criacao do projeto e tela de menu
    Projeto foi criado, pastas criadas dentro de Assets e a tela de menu simples funcional inserida na pasta Scenes.
    Criado SalamandraController e ScenesController.
    Build Settings modificado para identificar as cenas de MainMenu e GameScene.

---
+++ Assets/SalamandraController.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SalamandraController : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private float impulse;
+    [SerializeField] private float gravity;
+
+    private float ground;
+    private bool jump;
+    private float ySpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ground = transform.position.y;
+        jump = false;
+        ySpeed = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(jump)
+        {
+            Jump();
+        }
+        else if(Input.GetKeyDown(KeyCode.Space))
+        {
+            jump = true;
+            ySpeed = impulse;
+        }
+
+        float dx = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
+        
+        float xMax = Camera.main.transform.position.x + Camera.main.orthographicSize * Camera.main.aspect;
+        float xMin = xMax - 2*(xMax - Camera.main.transform.position.x);
+        if(transform.position.x + dx <= xMin || transform.position.x + dx >= xMax)
+            dx = 0;
+        
+        transform.Translate(dx, ySpeed * Time.deltaTime, 0);
+    }
+
+    void Jump()
+    {
+        if(transform.position.y <= ground)
+        {
+            transform.position.Set(transform.position.x, ground, 0);
+            jump = false;
+            ySpeed = 0;
+        }
+        else
+            ySpeed -= gravity * Time.deltaTime;
+    }
+}
---
+++ Assets/ScenesController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScenesController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void MainTitle()
+    {
+        SceneManager.LoadScene("Main Title");
+    }
+
+    public void QuitGame()
+    {
+        UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
